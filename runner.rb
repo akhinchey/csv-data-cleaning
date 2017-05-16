268b6c8070b3265d7d1327b1c29211987a1ff756
Amanda Hinchey <akhinchey@gmail.com>
2017-05-16 17:42:15 -0400

    Create method to call all Entry class methods.

--- runner.rb
+++ runner.rb
@@ -19,50 +19,18 @@ def save(filename, arr_or_entries)
   end
 end
 
-def first_words(arr_of_entries)
-  arr_of_entries.each do |entry|
-    entry.check_first_words 
-  end
-end
 
-def first_and_second_words(arr_of_entries)
+def clean_descriptions(arr_of_entries)
   arr_of_entries.each do |entry|
+    entry.check_first_words
     entry.check_first_and_second_words
-  end
-end
-
-def check_mid_keywords(arr_of_entries)
-  arr_of_entries.each do |entry|
     entry.check_for_mid_keywords
-  end
-end
-
-def check_for_periods(arr_of_entries)
-  arr_of_entries.each do |entry|
+    entry.capitalize if entry.is_title_cased?
     entry.capitalize_sentences
-  end
-end
-
-# method stays in runner(?)
-def capitalize_remainder(arr_of_entries)
-  arr_of_entries.select { |entry| entry.is_title_cased? }.each do |entry|
-      entry.capitalize
-    end
-end
-
-def up_acronyms(arr_of_entries)
-  arr_of_entries.each do |entry|
     entry.upcase_acronyms
-  end
-end
-
-def cap_words(arr_of_entries)
-  arr_of_entries.each do |entry|
     entry.capitalize_words
   end
 end
-
-
 
 
 
@@ -72,20 +40,7 @@ parse("jr_data_engineer_assignment.csv", entries)
 
 title_cased_entries = entries.select { |entry| entry.is_title_cased? }
 
-
-first_words(title_cased_entries)
-
-first_and_second_words(title_cased_entries)
-
-check_mid_keywords(title_cased_entries)
-
-capitalize_remainder(title_cased_entries)
-
-check_for_periods(title_cased_entries)
-
-up_acronyms(title_cased_entries)
-
-cap_words(title_cased_entries)
+clean_descriptions(title_cased_entries)
 
 title_cased_entries.each {|entry| puts entry.description}
 
