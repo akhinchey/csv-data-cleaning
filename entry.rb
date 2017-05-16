6bfa4c684f1a1706c3bab44fc7098b5e491278b7
Amanda Hinchey <akhinchey@gmail.com>
2017-05-16 17:27:42 -0400

    Move majority of methods to Entry.

--- entry.rb
+++ entry.rb
@@ -15,4 +15,98 @@ class Entry
     true
   end
 
+  def check_first_words
+    first_word = description.split(" ")[0]
+    if first_word == "Provides" || 
+      first_word == "Offers" ||
+      first_word == "A" ||
+      first_word == "An" ||
+      first_word == "Provide" ||
+      first_word == "Program" ||
+      first_word == "Offer" ||
+      first_word[-1] == "s"
+      description.capitalize!
+    end
+  end
+
+  def check_first_and_second_words
+    first_word = description.split(" ")[0]
+    second_word = description.split(" ")[1]
+
+    if first_word + " " + second_word == "Support Group" 
+      description.capitalize!
+    end
+  end
+
+  def check_for_mid_keywords
+    words_to_check_for = [ "Provides", 
+                            "Is", 
+                            "Offers", 
+                            "Offer", 
+                            "Serves", 
+                            "Sells", 
+                            "Helps", 
+                            "Strives", 
+                            "Distributes", 
+                            "Handles", 
+                            "Assists"
+                          ]
+
+    words = @description.split(" ")
+    key_word = words.find { |word| words_to_check_for.include?(word) }
+
+    if key_word && words.index(key_word) != 0 && words.index(key_word) < 6
+      index = words.index(key_word)
+      @description = words[0...index].join(" ") + " " + words[index..-1].join(" ").downcase
+    end
+  end
+
+  def capitalize
+    description.capitalize!
+  end
+
+
+  def capitalize_sentences
+    sentences = @description.split(". ")
+    sentences.each do |sentence|
+      sentence[0] = sentence[0].upcase
+    end
+    @description = sentences.join(". ")
+  end
+
+
+  def upcase_acronyms
+  acronyms = ["NYC", "GED", "HIV", "AIDS", "CPR"]
+      words = description.split(" ")
+      words.each do |word|
+        if acronyms.any? { |acronym| word.upcase == acronym }
+          description.gsub!(word, word.upcase)
+        end
+      end
+  end
+
+  def capitalize_words
+  words_to_check_for = [ "Sunday",
+                          "Monday",
+                          "Tuesday",
+                          "Wednesday",
+                          "Thursday",
+                          "Friday",
+                          "Saturday",
+                          "Jesus",
+                          "Christ",
+                          "Baptist",
+                          "Medicaid",
+                          "Medicare"
+                          ]
+
+    words = description.split(" ")
+    words.each do |word|
+      if words_to_check_for.any? { |cap_word| word.capitalize.include?(cap_word) }
+        description.gsub!(word, word.capitalize)
+      end
+    end
+  end
+
+
 end
--- runner.rb
+++ runner.rb
@@ -19,95 +19,46 @@ def save(filename, arr_or_entries)
   end
 end
 
-def check_first_words(arr_of_entries)
+def first_words(arr_of_entries)
   arr_of_entries.each do |entry|
-    first_word = entry.description.split(" ")[0]
-    if first_word == "Provides" || 
-      first_word == "Offers" ||
-      first_word == "A" ||
-      first_word == "An" ||
-      first_word == "Provide" ||
-      first_word == "Program" ||
-      first_word == "Offer" ||
-      first_word[-1] == "s"
-      entry.description.capitalize!
-    end
+    entry.check_first_words 
   end
 end
 
-def check_first_and_second_words(arr_of_entries)
+def first_and_second_words(arr_of_entries)
   arr_of_entries.each do |entry|
-    first_word = entry.description.split(" ")[0]
-    second_word = entry.description.split(" ")[1]
-
-    if first_word + " " + second_word == "Support Group" 
-      entry.description.capitalize!
-    end
+    entry.check_first_and_second_words
   end
 end
 
-def check_for_mid_keywords(arr_of_entries)
-  words_to_check_for = ["Provides", "Is", "Offers", "Offer", "Serves", "Sells", "Helps", "Strives", "Distributes", "Handles", "Assists"]
+def check_mid_keywords(arr_of_entries)
   arr_of_entries.each do |entry|
-    words = entry.description.split(" ")
-    key_word = words.find { |word| words_to_check_for.include?(word) }
-
-    if key_word && words.index(key_word) != 0 && words.index(key_word) < 6
-      index = words.index(key_word)
-      entry.description = words[0...index].join(" ") + " " + words[index..-1].join(" ").downcase
-    end
+    entry.check_for_mid_keywords
   end
 end
 
 def check_for_periods(arr_of_entries)
   arr_of_entries.each do |entry|
-    sentences = entry.description.split(". ")
-    sentences.each do |sentence|
-      sentence[0] = sentence[0].upcase
-    end
-    entry.description = sentences.join(". ")
+    entry.capitalize_sentences
   end
 end
 
+# method stays in runner(?)
 def capitalize_remainder(arr_of_entries)
   arr_of_entries.select { |entry| entry.is_title_cased? }.each do |entry|
-      entry.description.capitalize!
+      entry.capitalize
     end
 end
 
-def upcase_acronyms(arr_of_entries)
-  acronyms = ["NYC", "GED", "HIV", "AIDS", "CPR"]
+def up_acronyms(arr_of_entries)
   arr_of_entries.each do |entry|
-    words = entry.description.split(" ")
-    words.each do |word|
-      if acronyms.any? { |acronym| word.upcase == acronym }
-        entry.description.gsub!(word, word.upcase)
-      end
-    end
+    entry.upcase_acronyms
   end
 end
 
-def capitalize_words(arr_of_entries)
-  words_to_check_for = [ "Sunday",
-                          "Monday",
-                          "Tuesday",
-                          "Wednesday",
-                          "Thursday",
-                          "Friday",
-                          "Saturday",
-                          "Jesus",
-                          "Christ",
-                          "Baptist",
-                          "Medicaid",
-                          "Medicare"
-                          ]
+def cap_words(arr_of_entries)
   arr_of_entries.each do |entry|
-    words = entry.description.split(" ")
-    words.each do |word|
-      if words_to_check_for.any? { |cap_word| word.capitalize.include?(cap_word) }
-        entry.description.gsub!(word, word.capitalize)
-      end
-    end
+    entry.capitalize_words
   end
 end
 
@@ -121,24 +72,22 @@ parse("jr_data_engineer_assignment.csv", entries)
 
 title_cased_entries = entries.select { |entry| entry.is_title_cased? }
 
-title_cased_entries.each {|entry| puts entry.description}
 
+first_words(title_cased_entries)
 
-check_first_words(title_cased_entries)
+first_and_second_words(title_cased_entries)
 
-check_first_and_second_words(title_cased_entries)
-
-check_for_mid_keywords(title_cased_entries)
+check_mid_keywords(title_cased_entries)
 
 capitalize_remainder(title_cased_entries)
 
 check_for_periods(title_cased_entries)
 
-upcase_acronyms(title_cased_entries)
+up_acronyms(title_cased_entries)
 
-capitalize_words(title_cased_entries)
+cap_words(title_cased_entries)
 
-# title_cased_entries.each {|entry| puts entry.description}
+title_cased_entries.each {|entry| puts entry.description}
 
 # save("practice.csv", title_cased_entries)
 
